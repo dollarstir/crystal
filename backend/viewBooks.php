e578df04dece294492919d91fc6970a69e7f62bd
Frederick Ennin <kpin463@gmail.com>
2022-09-27 01:22:29 +0000

    dsadas

--- backend/viewBooks.php
+++ backend/viewBooks.php
@@ -33,7 +33,7 @@ adminhead(' View Product '); ?>
                                                 <div class="search_inner">
                                                     <form Active="#" class="">
                                                         <div class="search_field">
-                                                            <input id="myInput" type="text" placeholder="Search book here...">
+                                                            <input id="myInput" type="text" placeholder="Search product here...">
                                                         </div>
                                                         <!-- <button type="submit"> <i class="ti-search"></i> </button> -->
                                                     </form>
